adf767059c687e3199ab1a9c301b21bdb3da88c0
geovana40 <pilantir.geovana@escola.pr.gov.br>
2025-11-07 08:39:47 -0300

    carousel.js

---
+++ js/carousel.js
@@ -0,0 +1,15 @@
+var btns = documento.querySelectorAll('.listaDeArtigos-slider-item');
+
+//Percorrer todos os botoes controladores
+btns.forEach(funtion(btn) {
+    btn.addEventlistener('click', function()) {
+
+        
+        //Remover class 'ativo' dos outros botoes
+    btns.forEach(function(btnRemoveClass)
+
+    }
+
+
+
+}
