2c1d1ae1db137fe7c18374ad82945269da25fc95
geovana40 <pilantir.geovana@escola.pr.gov.br>
2025-11-07 11:15:56 -0300

    dialog

---
+++ js/dialog.js
@@ -0,0 +1,20 @@
+//variáveis
+var btnAbreDialog = document.querySelector('#abreDialog');
+var dialog = document.querySelector('.dialogNewsletter');
+var dialogBody = document.querySelector('.dialogNewsletter-body')
+var dialogOverlay = document.querySelector('dialogNewsletter-overlay')
+
+
+// quando abrir a dialog...
+btnAbreDialog.addEventListener('click', function() {
+    dialog.classList.add('dialogNewsletter--aberto');
+});
+
+function fechandoDialog () {
+    document.activeElement.getBoundingClientRect();
+    dialog.classList.add('dialogNewsletter--aberto');
+}
+
+//listeners
+
+document.querySelector('.dialogNewsletter-fechar').addEventListener('click', fechandoDialog);
